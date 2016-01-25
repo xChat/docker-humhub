e1cb324b13fc0a30cec48a83dfd82636c3c508da
Jerry <jerry.li.love@gmail.com>
2016-01-25 06:24:40 -0500

    Update pre-conf.sh
    Thanks to https://github.com/cboulanger/humhub-docker/blob/master/pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -10,4 +10,11 @@ mysqladmin -u root -p${DB_PASSWORD} create ${DB_DATABASE}
 echo "GRANT ALL ON ${DB_DATABASE}.* TO ${DB_USER}@localhost IDENTIFIED BY '${DB_PASSWORD}'; flush privileges; " | mysql -u root -p${DB_PASSWORD}
 
 killall mysqld
+echo "Database initialized ..."
 sleep 10s
+
+# install cron jobs for user www-data
+(crontab -u www-data -l ; echo "30 * * * * /var/www/humhub/protected/yii cron/hourly >/dev/null 2>&1") | crontab -u www-data -
+(crontab -u www-data -l ; echo "00 18 * * * /var/www/humhub/protected/yii cron/daily >/dev/null 2>&1") | crontab -u www-data -
+echo "Installed cron jobs ..." 
+
