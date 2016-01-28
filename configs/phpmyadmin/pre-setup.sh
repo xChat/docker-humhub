eaa21dc1ffaa6451ffb255251adf6d783d03aeee
Jerry <jerry.li.love@gmail.com>
2016-01-28 22:10:12 +1100

    Create pre-setup.sh

---
+++ configs/phpmyadmin/pre-setup.sh
@@ -0,0 +1,6 @@
+echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections)
+echo 'phpmyadmin phpmyadmin/app-password password${DB_ROOT_PASSWORD}' | debconf-set-selections)
+echo 'phpmyadmin phpmyadmin/app-password-confirm password ${DB_ROOT_PASSWORD}' | debconf-set-selections)
+echo 'phpmyadmin phpmyadmin/mysql/admin-pass password ${DB_ROOT_PASSWORD}' | debconf-set-selections)
+echo 'phpmyadmin phpmyadmin/mysql/app-pass password ${DB_ROOT_PASSWORD}' | debconf-set-selections)
+echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections)
