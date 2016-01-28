84e8aa034f1bbc4314c4686481b687ec7f84c343
Jerry <jerry.li.love@gmail.com>
2016-01-28 22:02:46 +1100

    Create pre-conf.sh

---
+++ configs/lamp/pre-conf.sh
@@ -0,0 +1,5 @@
+echo 'root:${ROOT_PASSWORD}' | chpasswd
+
+echo 'mysql-server mysql-server/root_password password ${DB_ROOT_PASSWORD}' | debconf-set-selections)
+echo 'mysql-server mysql-server/root_password_again password ${DB_ROOT_PASSWORD}' | debconf-set-selections)
+apt-get --yes --force-yes install lamp-server^
