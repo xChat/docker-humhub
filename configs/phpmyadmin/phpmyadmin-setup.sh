f2d9beddb7c0d2900618b3f67ba81d52605b1260
Jerry <jerry.li.love@gmail.com>
2016-01-28 16:00:29 -0500

    Update phpmyadmin-setup.sh

--- configs/phpmyadmin/phpmyadmin-setup.sh
+++ configs/phpmyadmin/phpmyadmin-setup.sh
@@ -8,15 +8,15 @@ RET=1
 while [[ RET -ne 0 ]]; do
     echo " ---> Waiting for MySQL"
     sleep 2
-    mysql -uroot -proot -e "status" > /dev/null 2>&1
+    mysql -uroot -p${DB_ROOT_PASSWORD} -e "status" > /dev/null 2>&1
     RET=$?
 done
 
 # Create the phpmyadmin storage configuration database.
-mysql -uroot -proot -e "CREATE DATABASE phpmyadmin; GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'root'@'localhost' IDENTIFIED BY 'root'; FLUSH PRIVILEGES;"
+mysql -uroot -p${DB_ROOT_PASSWORD} -e "CREATE DATABASE phpmyadmin; GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'root'@'localhost' IDENTIFIED BY 'root'; FLUSH PRIVILEGES;"
 
 # Import the configuration storage database.
 gunzip < /usr/share/doc/phpmyadmin/examples/create_tables.sql.gz | mysql -u root -proot phpmyadmin
 
 # Shutdown the server.
-mysqladmin -u root -proot shutdown
+mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
