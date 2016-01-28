6df63c2b5e09359d9f58ad99ba09dddef2b9cb95
Jerry <jerry.li.love@gmail.com>
2016-01-28 15:48:56 -0500

    Update pre-phpmyadmin-setup.sh

--- configs/phpmyadmin/pre-phpmyadmin-setup.sh
+++ configs/phpmyadmin/pre-phpmyadmin-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections)
-echo 'phpmyadmin phpmyadmin/app-password password${DB_ROOT_PASSWORD}' | debconf-set-selections)
-echo 'phpmyadmin phpmyadmin/app-password-confirm password ${DB_ROOT_PASSWORD}' | debconf-set-selections)
-echo 'phpmyadmin phpmyadmin/mysql/admin-pass password ${DB_ROOT_PASSWORD}' | debconf-set-selections)
-echo 'phpmyadmin phpmyadmin/mysql/app-pass password ${DB_ROOT_PASSWORD}' | debconf-set-selections)
-echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections)
+echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/app-password password${DB_ROOT_PASSWORD}' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/app-password-confirm password ${DB_ROOT_PASSWORD}' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/admin-pass password ${DB_ROOT_PASSWORD}' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/app-pass password ${DB_ROOT_PASSWORD}' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections
