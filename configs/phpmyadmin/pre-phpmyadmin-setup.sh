be476a96bb928fcc69dbe8167700a58f6aa85a74
Jerry <jerry.li.love@gmail.com>
2016-02-02 09:26:27 +1100

    Update pre-phpmyadmin-setup.sh

--- configs/phpmyadmin/pre-phpmyadmin-setup.sh
+++ configs/phpmyadmin/pre-phpmyadmin-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
-echo 'phpmyadmin phpmyadmin/app-password password${DB_ROOT_PASSWORD}' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/app-password password ${DB_ROOT_PASSWORD}' | debconf-set-selections
 echo 'phpmyadmin phpmyadmin/app-password-confirm password ${DB_ROOT_PASSWORD}' | debconf-set-selections
 echo 'phpmyadmin phpmyadmin/mysql/admin-pass password ${DB_ROOT_PASSWORD}' | debconf-set-selections
 echo 'phpmyadmin phpmyadmin/mysql/app-pass password ${DB_ROOT_PASSWORD}' | debconf-set-selections
