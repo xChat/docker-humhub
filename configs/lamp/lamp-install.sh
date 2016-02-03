f330129e9955e73367313ac80a89c128c4df7692
Jerry <jerry.li.love@gmail.com>
2016-02-03 23:15:34 +1100

    Update lamp-install.sh

--- configs/lamp/lamp-install.sh
+++ configs/lamp/lamp-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo 'CREATE DATABASE' ${DB_DATABASE}';'
+exit 1;
+
 echo 'mysql-server mysql-server/root_password password' ${DB_ROOT_PASSWORD} | debconf-set-selections
 echo 'mysql-server mysql-server/root_password_again password' ${DB_ROOT_PASSWORD} | debconf-set-selections
 apt-get --yes --force-yes install lamp-server^
