1c7ea4737e8910c872b50c950b60b62cef3f16ce
Jerry <jerry.li.love@gmail.com>
2016-02-03 23:35:17 +1100

    Update lamp-install.sh

--- configs/lamp/lamp-install.sh
+++ configs/lamp/lamp-install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo 'CREATE DATABASE' ${DB_DATABASE}';'
-echo "GRANT ALL ON "${DB_DATABASE}".* TO '"${DB_USER}"'@'localhost' IDENTIFIED BY '"${DB_PASSWORD}"'; flush privileges;"
-exit 1;
+# debug only
+#echo 'CREATE DATABASE' ${DB_DATABASE}';'
+#echo "GRANT ALL ON "${DB_DATABASE}".* TO '"${DB_USER}"'@'localhost' IDENTIFIED BY '"${DB_PASSWORD}"'; flush privileges;"
+#exit 1;
 
 echo 'mysql-server mysql-server/root_password password' ${DB_ROOT_PASSWORD} | debconf-set-selections
 echo 'mysql-server mysql-server/root_password_again password' ${DB_ROOT_PASSWORD} | debconf-set-selections
