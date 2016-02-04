44b9356b3a0b1e6bc7efbc52a829aa9ba73eda50
Jerry <jerry.li.love@gmail.com>
2016-02-04 11:30:37 +1100

    Update phpmyadmin-setup.sh

--- configs/phpmyadmin/phpmyadmin-setup.sh
+++ configs/phpmyadmin/phpmyadmin-setup.sh
@@ -13,7 +13,7 @@ while [[ RET -ne 0 ]]; do
 done
 
 # create the phpmyadmin storage configuration database.
-echo "CREATE DATABASE phpmyadmin; GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'root'@'localhost' IDENTIFIED BY 'root'; FLUSH PRIVILEGES;" | mysql -uroot -p${DB_ROOT_PASSWORD}
+echo "CREATE DATABASE phpmyadmin; GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'root'@'localhost' IDENTIFIED BY '"${DB_ROOT_PASSWORD}"'; FLUSH PRIVILEGES;" | mysql -uroot -p${DB_ROOT_PASSWORD}
 
 # import the configuration storage database.
 gunzip < /usr/share/doc/phpmyadmin/examples/create_tables.sql.gz | mysql -uroot -p${DB_ROOT_PASSWORD} phpmyadmin
